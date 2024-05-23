13c83ecedb079b9e2060cb2defb0f037de451516
Sudip Subedi <sudip.subedi99@gmail.com>
2024-05-23 13:54:21 +0545

    advanced indexing

--- 05 LoadDataFromFil.py
+++ 05 LoadDataFromFil.py
@@ -5,3 +5,10 @@ import numpy as np
 
 fileData = np.genfromtxt('data.txt', delimiter=' ', dtype=None, encoding=None)
 print(fileData)
+
+print(fileData>50)
+print(fileData[fileData>50])
+print(np.any(fileData>50, axis=0))
+
+a = np.array([2,3,4,5,6,7,8,9])
+print(a[[2,3,4]])
